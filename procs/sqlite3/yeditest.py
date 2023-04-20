34a7e796c4b231c7a4eb11e7a35452217af0ea67
jabibo <github@binge.de>
2023-04-20 10:21:13 +0200

    .

--- procs/sqlite3/yeditest.py
+++ procs/sqlite3/yeditest.py
@@ -74,6 +74,7 @@ def gen_target_objects(cursor,source, hashdiff_naming):
                 WHERE src.Source_System = '{source_name}' 
                 and src.Source_Object = '{source_object}'
                 and not coalesce(is_ref_object, false)
+                
                 ORDER BY h.Target_Column_Sort_Order
               ) 
               GROUP BY Target_Primary_Key_Physical_Name
@@ -99,6 +100,7 @@ def gen_target_objects(cursor,source, hashdiff_naming):
                   on l.Source_Table_Identifier = src.Source_table_identifier
                 WHERE src.Source_System = '{source_name}' 
                 and src.Source_Object = '{source_object}'
+                
                 ORDER BY l.Target_Column_Sort_Order
               )
               group by Target_Primary_Key_Physical_Name
@@ -124,6 +126,7 @@ def gen_target_objects(cursor,source, hashdiff_naming):
                   on l.Source_Table_Identifier = src.Source_table_identifier
                 WHERE src.Source_System = '{source_name}' 
                   and src.Source_Object = '{source_object}'
+                  
                 ORDER BY l.Target_Column_Sort_Order
               )
               group by target_column_physical_name              
@@ -149,6 +152,7 @@ def gen_target_objects(cursor,source, hashdiff_naming):
                   on s.Source_Table_Identifier = src.Source_table_identifier
                 WHERE src.Source_System = '{source_name}' 
                   and src.Source_Object = '{source_object}'
+                  
                 order by s.Target_Column_Sort_Order
               )
               group by Target_Satellite_Table_Physical_Name
@@ -174,6 +178,7 @@ def gen_target_objects(cursor,source, hashdiff_naming):
                 on s.Source_Table_Identifier = src.Source_table_identifier
               WHERE src.Source_System = '{source_name}' 
                 and src.Source_Object = '{source_object}'
+                
               order by s.Target_Column_Sort_Order)
               group by Target_Satellite_Table_Physical_Name
               """
@@ -221,158 +226,6 @@ def gen_target_objects(cursor,source, hashdiff_naming):
 
   return target_model_def
 
-def gen_multi_active_config(cursor,source):
-  
-  command = ""
-
-  source_name, source_object = helper.source_split(source)
-
-  query = f"""
-              SELECT 
-                  s.source_table_identifier
-                  ,s.target_satellite_table_physical_name 
-                  , s.hub_primary_key_physical_name 
-                  , group_concat(s.target_column_physical_name) target_column_physical_name
-              FROM hub_satellites s
-              inner join source_data src 
-                on s.Source_Table_Identifier = src.Source_table_identifier
-              WHERE src.Source_System = '{source_name}' 
-                and src.Source_Object = '{source_object}'
-                and  ma_attribute
-              """
-  cursor.execute(query)
-  results = cursor.fetchall()
-  # print(results)
-  if not results:
-    # print("not:",results)
-    return ""
-  command = ""
-
-  for multi_active_config in results:
-    if any(item is None for item in multi_active_config):
-        continue    
-    command += "multi_active_config:\n\t\tmulti_active_key:\n"    
-    main_hashkey_column = multi_active_config[2]
-    multi_active_key_list = multi_active_config[3].split(",")
-
-
-    for multi_active_key in multi_active_key_list:
-      command += f"\t\t\t- {multi_active_key}\n"  
-
-    command +=  f"\t\tmain_hashkey_column: {main_hashkey_column}\n"
-    
-  # print(command)
-  
-  return command
-
-
-
-
-def gen_derived_columns(cursor,source):
-  
-  command = ""
-
-  source_name, source_object = helper.source_split(source)
-
-  query = f"""
-   SELECT 
-              group_concat(source_column_physical_name), target_column_physical_name, transformation_rule  
-              from
-              (              
-              SELECT 
-              case when transformation_rule<>''
-                     then transformation_rule
-                     else source_column_physical_name end as source_column_physical_name 
-              , target_column_physical_name
-              , case when transformation_rule<>''
-                     then True
-                     else False end as transformation_rule
-              FROM hub_satellites s
-              inner join source_data src on s.Source_Table_Identifier = src.Source_table_identifier
-              WHERE src.Source_System = '{source_name}' and src.Source_Object = '{source_object}'
-              and source_column_physical_name<>target_column_physical_name 
-              union
-              SELECT 
-              source_column_physical_name 
-              , target_column_physical_name  
-              , False as transformation_rule              
-              FROM link_satellites s
-              inner join source_data src on s.Source_Table_Identifier = src.Source_table_identifier
-              WHERE src.Source_System = '{source_name}' and src.Source_Object = '{source_object}'
-              and source_column_physical_name<>target_column_physical_name
-              union
-              SELECT distinct
-              case when transformation_rule<>''
-                     then transformation_rule
-                     else source_column_physical_name end as source_column_physical_name
-              , business_key_physical_name 
-              , case when transformation_rule<>''
-                     then True
-                     else False end as transformation_rule
-              FROM hub_entities s
-              inner join source_data src on s.Source_Table_Identifier = src.Source_table_identifier
-              WHERE src.Source_System = '{source_name}' and src.Source_Object = '{source_object}'
-              and source_column_physical_name<>business_key_physical_name          
-              )
-              group by target_column_physical_name  
-              """
-  cursor.execute(query)
-  results = cursor.fetchall()
-  for derived_columns in results:
-    target_column_name = derived_columns[1]
-    transformation_rule = derived_columns[2]
-    command = command + f"\t\t{target_column_name}:\n"
-    if transformation_rule:
-      source_column_list = derived_columns[0]
-      command = command + f"\t\t\tvalue: {source_column_list}"      
-    else:
-      source_column_list = derived_columns[0].split(",")
-      for i, source_column in enumerate(source_column_list):
-          if i == 0:
-            command = command + f"\t\t\tvalue: {source_column}"
-          else:
-            command += f"||'_'||{source_column}"
-    command = command + f"\n\t\t\tdatatype: 'VARCHAR'\n"
-  return command
-
-
-def gen_prejoin_columns(cursor, source):
-  
-  command = ""  
-
-  source_name, source_object = helper.source_split(source)
-  
-  query = f"""SELECT 
-              COALESCE(l.Prejoin_Target_Column_Alias,l.Prejoin_Extraction_Column_Name) as Prejoin_Target_Column_Name,
-              pj_src.Source_Schema_Physical_Name, 
-              pj_src.Source_Table_Physical_Name,
-              l.Prejoin_Extraction_Column_Name, 
-              l.Source_column_physical_name,
-              l.Prejoin_Table_Column_Name
-              FROM link_entities l
-              inner join source_data src on l.Source_Table_Identifier = src.Source_table_identifier
-              inner join source_data pj_src on l.Prejoin_Table_Identifier = pj_src.Source_table_identifier
-              WHERE src.Source_System = '{source_name}' and src.Source_Object = '{source_object}'
-              and l.Prejoin_Table_Identifier is not NULL"""
-  
-  
-  cursor.execute(query)
-  prejoined_column_rows = cursor.fetchall()
-  for prejoined_column in prejoined_column_rows:
-
-    if command == "":
-      command = "prejoined_columns:\n"
-
-    schema = prejoined_column[1]
-    table = prejoined_column[2]
-    alias = prejoined_column[0]
-    bk_column = prejoined_column[3]
-    this_column_name = prejoined_column[4]
-    ref_column_name = prejoined_column[5]
-
-    command = command + f"""\t{alias}:\n\t\tsrc_schema:"{schema}"\n\t\tsrc_table:"{table}"\n\t\tbk:"{bk_column}"\n\t\tthis_column_name:"{this_column_name}"\n\t\tref_column_name:"{ref_column_name}"\n"""
-
-  return command
   
 
 def generate_yeditest(cursor, source,generated_timestamp,stage_default_schema, model_path,hashdiff_naming):
@@ -380,12 +233,6 @@ def generate_yeditest(cursor, source,generated_timestamp,stage_default_schema, m
   target_object_model = ""
   target_object_model = gen_target_objects(cursor, source, hashdiff_naming)
   
-  multi_active_config = gen_multi_active_config(cursor, source)
-
-  derived_columns = gen_derived_columns(cursor, source)
-
-  prejoins = gen_prejoin_columns(cursor, source)
-
   source_name, source_object = helper.source_split(source)
   # print(source_name + ':' + source_object)
   
@@ -393,7 +240,9 @@ def generate_yeditest(cursor, source,generated_timestamp,stage_default_schema, m
 
   query = f"""SELECT Source_Schema_Physical_Name,Source_Table_Physical_Name, Record_Source_Column, Load_Date_Column, source_object, load_completeness_type
               FROM source_data src
-              WHERE src.Source_System = '{source_name}' and src.Source_Object = '{source_object}'"""
+              WHERE src.Source_System = '{source_name}' and src.Source_Object = '{source_object}'
+              and src.JediTest == 'True'
+              """
   cursor.execute(query)
   sources = cursor.fetchall()
   for row in sources: #sources usually only has one row
